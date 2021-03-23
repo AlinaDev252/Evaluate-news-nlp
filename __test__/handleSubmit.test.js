cd49d307bf5fffa40ab2ee41c769a3485a1c53e6
AlinaDev252 <costachealina25@gmail.com>
2021-03-23 14:40:41 +0200

    Revise test for handleSubmit function

--- __test__/handleSubmit.test.js
+++ __test__/handleSubmit.test.js
@@ -1,5 +1,7 @@
 import { handleSubmit } from "../src/client/js/formHandler";
 import { mockAxios } from "axios";
+import axios from "axios";
+let Axios;
 let MockAdapter;
 
 describe("Testing if the function is not null", () => {
@@ -10,19 +12,24 @@ describe("Testing if the function is not null", () => {
 
 //Mock event.preventDefault() with Jest
 
-describe("Mocking preventDefault", () => {
-	test("It should mock preventDefault", () => {
-		const event = { preventDefault: () => {} };
+// describe("Mocking preventDefault", () => {
+// 	test("It should mock preventDefault", () => {
+// 		const event = { preventDefault: () => {} };
 
-		// runs before each test starts running
-		beforeEach(() => {
-			mockAxios = new MockAdapter(Axios);
-			jest.spyOn(event, "preventDefault");
-		});
+// 		// runs before each test starts running
+// 		beforeEach(() => {
+// 			// mockAxios = new MockAdapter(Axios);
+// 			jest.spyOn(event, "preventDefault");
+// 		});
 
-		// runs after each test has finished
-		afterEach(() => {
-			mockAxios.reset();
-		});
-	});
+// 		// runs after each test has finished
+// 		afterEach(() => {
+// 			mockAxios.reset();
+// 		});
+// 	});
+// });
+
+test("the data is irony", async () => {
+	const data = await handleSubmit();
+	expect(data).toBe("irony");
 });
